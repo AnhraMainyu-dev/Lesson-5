a02485adb992d378694028062e8b2732e37a10d9
AnhraMainyu-dev <maximheilelov@gmail.com>
2026-01-16 20:16:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+import file_operations
+import random
+import os
+from faker import Faker
+
+
+RUNIC_DICTIONARY = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+SKILLS = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд"]
+
+
+def main():
+    if not os.path.exists("output/RPGCharacters"):
+        os.makedirs("output/RPGCharacters")
+    fake = Faker("ru_RU")
+    runic_skills = []
+    for skill in SKILLS:
+        new_skill = ""
+        for sign in skill:
+            for rune in RUNIC_DICTIONARY:
+                if sign == rune:
+                    new_skill = new_skill + RUNIC_DICTIONARY[rune]
+        runic_skills.append(new_skill)
+
+    for count in range(10):
+        random_runic_skill = random.sample(runic_skills, 3)
+        context = {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": random_runic_skill[0],
+            "skill_2": random_runic_skill[1],
+            "skill_3": random_runic_skill[2]
+        }
+        file_operations.render_template(
+            "src/template.svg",
+            "output/RPGCharacters/Character № {}.svg".format(count),
+            context
+        )
+
+
+if __name__ == '__main__':
+    main()
